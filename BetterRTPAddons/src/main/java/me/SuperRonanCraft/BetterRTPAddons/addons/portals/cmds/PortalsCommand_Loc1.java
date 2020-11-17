4682c50080f839e530fa5ee8ee0ae9d6831cc4c2
SuperRonanCraft <SuperRonanCraft@gmail.com>
2020-11-17 11:59:11 -0500

    protocol lib added

--- BetterRTPAddons/src/main/java/me/SuperRonanCraft/BetterRTPAddons/addons/portals/cmds/PortalsCommand_Loc1.java
+++ BetterRTPAddons/src/main/java/me/SuperRonanCraft/BetterRTPAddons/addons/portals/cmds/PortalsCommand_Loc1.java
@@ -1,12 +1,21 @@
 package me.SuperRonanCraft.BetterRTPAddons.addons.portals.cmds;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.WrappedBlockData;
 import me.SuperRonanCraft.BetterRTPAddons.addons.portals.AddonPortals;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class PortalsCommand_Loc1 implements PortalsCommands, LocationFinder {
+
+    private PacketContainer fakeBlock;
 
     @Override
     public void execute(CommandSender sendi, String label, String[] args, AddonPortals addonPortals) {
@@ -14,6 +23,17 @@ public class PortalsCommand_Loc1 implements PortalsCommands, LocationFinder {
         Location loc = getTargetBlock(p, 10).getLocation();
         addonPortals.getPortals().setPortal(p, loc, false);
         sendi.sendMessage("Location 1 set to this location " + loc.toString());
-        p.sendBlockChange(loc, Material.GLOWSTONE, (byte) 0);
+        //p.sendBlockChange(loc, Material.GLOWSTONE, (byte) 0);
+
+        ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+        WrapperPlayServerBlockChange fakeBlock = new WrapperPlayServerBlockChange();
+        fakeBlock.setLocation(BlockLocation);
+        try {
+            //System.out.println(fakeBlock.toString());
+            protocolManager.sendServerPacket(p, fakeBlock);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(
+                    "Cannot send packet " + fakeBlock, e);
+        }
     }
 }
---
+++ BetterRTPAddons/src/main/java/me/SuperRonanCraft/BetterRTPAddons/addons/portals/cmds/WrapperPlayServerBlockChange.java
@@ -0,0 +1,70 @@
+package me.SuperRonanCraft.BetterRTPAddons.addons.portals.cmds;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.BlockPosition;
+import com.comphenix.protocol.wrappers.WrappedBlockData;
+
+public class WrapperPlayServerBlockChange extends AbstractPacket {
+    public static final PacketType TYPE = PacketType.Play.Server.BLOCK_CHANGE;
+
+    public WrapperPlayServerBlockChange() {
+        super(new PacketContainer(TYPE), TYPE);
+        handle.getModifier().writeDefaults();
+    }
+
+    public WrapperPlayServerBlockChange(PacketContainer packet) {
+        super(packet, TYPE);
+    }
+
+    /**
+     * Retrieve Location.
+     * <p>
+     * Notes: block Coordinates
+     *
+     * @return The current Location
+     */
+    public BlockPosition getLocation() {
+        return handle.getBlockPositionModifier().read(0);
+    }
+
+    /**
+     * Set Location.
+     *
+     * @param value - new value.
+     */
+    public void setLocation(BlockPosition value) {
+        handle.getBlockPositionModifier().write(0, value);
+    }
+
+    /**
+     * Retrieve the Bukkit Location.
+     *
+     * @param world World for the location
+     * @return Bukkit Location
+     */
+    public Location getBukkitLocation(World world) {
+        return getLocation().toVector().toLocation(world);
+    }
+
+    /**
+     * Retrieve Block Data.
+     *
+     * @return The current Block Data
+     */
+    public WrappedBlockData getBlockData() {
+        return handle.getBlockData().read(0);
+    }
+
+    /**
+     * Set Block Data.
+     *
+     * @param value - new value.
+     */
+    public void setBlockData(WrappedBlockData value) {
+        handle.getBlockData().write(0, value);
+    }
+}
