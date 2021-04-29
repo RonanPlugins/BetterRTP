b6e42c12775794affe2fabfaa541710051dccd6d
SuperRonanCraft <SuperRonanCraft@gmail.com>
2021-04-29 11:49:05 -0400

    added a player parameter to locations command

--- src/main/java/me/SuperRonanCraft/BetterRTP/player/commands/types/CmdLocation.java
+++ src/main/java/me/SuperRonanCraft/BetterRTP/player/commands/types/CmdLocation.java
@@ -23,7 +23,7 @@ public class CmdLocation implements RTPCommand, RTPCommandHelpable {
         return "location";
     }
 
-    //rtp location <location name>
+    //rtp location <location name> [player]
     public void execute(CommandSender sendi, String label, String[] args) {
         if (args.length == 2) {
             if (sendi instanceof  Player) {
@@ -38,6 +38,21 @@ public class CmdLocation implements RTPCommand, RTPCommandHelpable {
                 usage(sendi, label);
             } else
                 sendi.sendMessage("Console is not able to execute this command! Try '/rtp help'");
+        } else if (args.length == 3 && BetterRTP.getInstance().getPerms().getRtpOther(sendi)) {
+            Player p = Bukkit.getPlayer(args[2]);
+            if (p != null && p.isOnline()) {
+                for (String location_name : getLocations().keySet()) {
+                    if (location_name.equalsIgnoreCase(args[1].toLowerCase())) {
+                        BetterRTP.getInstance().getCmd().tp(p, sendi, null, null, RTP_TYPE.COMMAND,
+                                false, false, (WorldLocations) getLocations().get(location_name));
+                        return;
+                    }
+                }
+                usage(sendi, label);
+            } else if (p != null)
+                getCmd().playerNotOnline(sendi, args[1]);
+            else
+                usage(sendi, label);
         } else
             usage(sendi, label);
     }
@@ -48,6 +63,10 @@ public class CmdLocation implements RTPCommand, RTPCommandHelpable {
             for (String location_name : getLocations().keySet())
                 if (location_name.toLowerCase().startsWith(args[1].toLowerCase()))
                     list.add(location_name);
+        } else if (args.length == 3 && BetterRTP.getInstance().getPerms().getRtpOther(sendi)) {
+            for (Player p : Bukkit.getOnlinePlayers())
+                if (p.getName().toLowerCase().startsWith(args[2].toLowerCase()))
+                    list.add(p.getName());
         }
         return list;
     }
@@ -62,6 +81,10 @@ public class CmdLocation implements RTPCommand, RTPCommandHelpable {
 
     private HashMap<String, RTPWorld> getLocations() {
         return BetterRTP.getInstance().getRTP().worldLocations;
+    }
+
+    private Commands getCmd() {
+        return BetterRTP.getInstance().getCmd();
     }
 
     @Override
