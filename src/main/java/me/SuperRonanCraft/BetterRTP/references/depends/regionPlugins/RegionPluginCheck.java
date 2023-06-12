34cace70f7a3e0a48beff89593ad13230d7839ed
TechnicallyCoded <technicallycoded@gmail.com>
2023-06-12 14:26:00 +0200

    Add more documentation to validator plugins

--- src/main/java/me/SuperRonanCraft/BetterRTP/references/depends/regionPlugins/RegionPluginCheck.java
+++ src/main/java/me/SuperRonanCraft/BetterRTP/references/depends/regionPlugins/RegionPluginCheck.java
@@ -4,6 +4,10 @@ import org.bukkit.Location;
 
 public interface RegionPluginCheck {
 
+    /**
+     * @param loc Location to check
+     * @return True if valid teleport location, false if the location is claimed and invalid
+     */
     boolean check(Location loc);
 
 }
