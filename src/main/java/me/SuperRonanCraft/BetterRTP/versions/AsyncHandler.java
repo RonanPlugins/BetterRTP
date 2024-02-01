947e31851ffd4b2e419b0435861c9d7a2bd82a9f
TechnicallyCoded <technicallycoded@gmail.com>
2024-02-01 15:32:15 +0100

    Update FoliaLib to 0.3.1

--- src/main/java/me/SuperRonanCraft/BetterRTP/versions/AsyncHandler.java
+++ src/main/java/me/SuperRonanCraft/BetterRTP/versions/AsyncHandler.java
@@ -1,29 +1,33 @@
 package me.SuperRonanCraft.BetterRTP.versions;
 
 import com.tcoded.folialib.impl.ServerImplementation;
-import com.tcoded.folialib.wrapper.WrappedTask;
+import com.tcoded.folialib.wrapper.task.WrappedTask;
 import me.SuperRonanCraft.BetterRTP.BetterRTP;
-
-import java.util.concurrent.TimeUnit;
+import org.bukkit.entity.Entity;
 
 public class AsyncHandler {
 
     public static void async(Runnable runnable) {
-        getFolia().runAsync(runnable);
+        getFolia().runAsync(task -> runnable.run());
         //Bukkit.getScheduler().runTaskAsynchronously(BetterRTP.getInstance(), runnable);
     }
 
     public static void sync(Runnable runnable) {
-        getFolia().runNextTick(runnable);
+        getFolia().runNextTick(task -> runnable.run());
+        //Bukkit.getScheduler().runTask(BetterRTP.getInstance(), runnable);
+    }
+
+    public static void syncAtEntity(Entity entity, Runnable runnable) {
+        getFolia().runAtEntity(entity, task -> runnable.run());
         //Bukkit.getScheduler().runTask(BetterRTP.getInstance(), runnable);
     }
 
     public static WrappedTask asyncLater(Runnable runnable, long ticks) {
-        return getFolia().runLaterAsync(runnable, ticks * 50, TimeUnit.MILLISECONDS);
+        return getFolia().runLaterAsync(runnable, ticks);
         //return Bukkit.getScheduler().runTaskLaterAsynchronously(BetterRTP.getInstance(), runnable, ticks);
     }
     public static WrappedTask syncLater(Runnable runnable, long ticks) {
-        return getFolia().runLater(runnable, ticks * 50, TimeUnit.MILLISECONDS);
+        return getFolia().runLater(runnable, ticks);
         //return Bukkit.getScheduler().runTaskLater(BetterRTP.getInstance(), runnable, ticks);
     }
 
