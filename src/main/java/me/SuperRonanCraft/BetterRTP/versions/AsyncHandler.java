82425c5be2ac09bf7bb414b79e46e3b8a2d211fc
Yomamaeatstoes <114538961+Yomamaeatstoes@users.noreply.github.com>
2026-05-07 03:57:43 -0400

    Add syncAtLocation and teleportAsync methods
    Added methods for synchronous execution at a location and asynchronous teleportation of entities.

--- src/main/java/me/SuperRonanCraft/BetterRTP/versions/AsyncHandler.java
+++ src/main/java/me/SuperRonanCraft/BetterRTP/versions/AsyncHandler.java
@@ -3,32 +3,39 @@ package me.SuperRonanCraft.BetterRTP.versions;
 import com.tcoded.folialib.impl.ServerImplementation;
 import com.tcoded.folialib.wrapper.task.WrappedTask;
 import me.SuperRonanCraft.BetterRTP.BetterRTP;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+
+import java.util.concurrent.CompletableFuture;
 
 public class AsyncHandler {
 
     public static void async(Runnable runnable) {
         getFolia().runAsync(task -> runnable.run());
-        //Bukkit.getScheduler().runTaskAsynchronously(BetterRTP.getInstance(), runnable);
     }
 
     public static void sync(Runnable runnable) {
         getFolia().runNextTick(task -> runnable.run());
-        //Bukkit.getScheduler().runTask(BetterRTP.getInstance(), runnable);
     }
 
     public static void syncAtEntity(Entity entity, Runnable runnable) {
         getFolia().runAtEntity(entity, task -> runnable.run());
-        //Bukkit.getScheduler().runTask(BetterRTP.getInstance(), runnable);
+    }
+
+    public static void syncAtLocation(Location location, Runnable runnable) {
+        getFolia().runAtLocation(location, task -> runnable.run());
+    }
+
+    public static CompletableFuture<Boolean> teleportAsync(Entity entity, Location location) {
+        return getFolia().teleportAsync(entity, location);
     }
 
     public static WrappedTask asyncLater(Runnable runnable, long ticks) {
         return getFolia().runLaterAsync(runnable, ticks);
-        //return Bukkit.getScheduler().runTaskLaterAsynchronously(BetterRTP.getInstance(), runnable, ticks);
     }
+
     public static WrappedTask syncLater(Runnable runnable, long ticks) {
         return getFolia().runLater(runnable, ticks);
-        //return Bukkit.getScheduler().runTaskLater(BetterRTP.getInstance(), runnable, ticks);
     }
 
     private static ServerImplementation getFolia() {
